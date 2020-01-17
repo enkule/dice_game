978596158af95ac0ee0762149b44cef4f69f7fd1
enkhjin <enkule.enkhjin@gmail.com>
2020-01-17 11:04:32 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var scores = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-window.document.querySelector("#score-0").textContent = 0;
+var diceDom = document.querySelector(".dice");
 
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+diceDom.style.display = "none";
 
-console.log("Шоо : " + dice);
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
