3318a77e3f284f323cfd8de0acb52093e4f0c6ee
enkhjin <enkule.enkhjin@gmail.com>
2020-01-17 16:02:31 +0800

    Hold tovchiig ajillagaatai bolgosn

--- app.js
+++ app.js
@@ -27,13 +27,38 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
     roundScore = roundScore + diceNumber;
     document.getElementById("current-" + activePlayer).textContent = roundScore;
   } else {
-    roundScore = 0;
-    document.getElementById("current-" + activePlayer).textContent = 0;
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    // ulaan tseg shiljuuleh toggle baival nemne bxgui bol hasna
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-    // shoog tur alga bolgono
-    diceDom.style.display = "none";
+    switchToNextPlayer();
   }
 });
+
+// hold tovchnii eventlistener
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  // toglogchiin eeljind tsugluulsan onoog global onoon deer nemj ugnu
+  scores[activePlayer] = scores[activePlayer] + roundScore;
+
+  // delgets deer onoog n uurchilnu
+
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  //hojih esehiig shalgah
+  if (scores[activePlayer] >= 100) {
+    document.getElementById("name-" + activePlayer).textContent = " WINNER!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    switchToNextPlayer();
+  }
+});
+// ene function ni togloh eeljiig daraagiin eeljruu shiljuulne
+function switchToNextPlayer() {
+  // eeljiin onoog butsaagad noillono
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  // ulaan tseg shiljuuleh toggle baival nemne bxgui bol hasna
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  // shoog tur alga bolgono
+  diceDom.style.display = "none";
+}
